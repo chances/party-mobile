0241d6152b81b370ca2b5f9c54b2680ed7e3497d
Chance Snow <git@chancesnow.me>
2017-10-15 20:04:14 -0700

    Add flutter_search_bar and integrate into Party page

--- lib/views/party_page.dart
+++ lib/views/party_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 
 import 'package:party/app_context.dart';
 import 'package:party/constants.dart';
@@ -21,7 +22,16 @@ class PartyPage extends StatefulWidget {
 }
 
 class _PartyPageState extends State<PartyPage> {
+  SearchBar searchBar;
   bool _loading = true;
+
+  _PartyPageState() {
+    searchBar = new SearchBar(
+        hintText: 'Search for a song',
+        setState: setState,
+        buildDefaultAppBar: buildAppBar
+    );
+  }
 
   Future<Null> get _loadUser {
     return null;
@@ -36,23 +46,54 @@ class _PartyPageState extends State<PartyPage> {
 
   @override
   Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: searchBar.build(context),
+      bottomNavigationBar: Constants.footer,
+      body: new Stack(children: buildStartParty(context)),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
     final actions = [
+      new IconButton(
+        icon: new Icon(Icons.add),
+        tooltip: 'Add a song',
+        onPressed: () {
+          searchBar.beginSearch(context);
+        },
+      ),
       Constants.logoutMenu(context)
     ];
 
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Your Party"),
-        actions: actions,
-        backgroundColor: Constants.statusBarColor,
-        elevation: 4.0,
-      ),
-      bottomNavigationBar: Constants.footer,
-      body: new Center(
-          child: new PrimaryButton(
-              'Select Playlist', onPressed: _selectPlaylist
-          )
-      ),
+    return new AppBar(
+      title: new Text("Your Party"),
+      backgroundColor: Constants.statusBarColor,
+      elevation: 4.0,
+      actions: actions,
+      bottom: null, // TODO: Tab switcher; 'Music', 'Guests', 'Games'
     );
+  }
+
+  List<Widget> buildStartParty(BuildContext context) {
+    return [
+      new Center(
+          child: new Column(
+            children: [
+              new Padding(
+                padding: new EdgeInsets.only(bottom: 16.0),
+                child: new Column(children: [
+                  new Text(
+                    'Host a party with music, games, and fun!',
+                    style: Theme.of(context).textTheme.subhead,
+                  )
+                ]),
+              ),
+              new PrimaryButton('Start', onPressed: _selectPlaylist)
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          )
+      )
+    ];
   }
 }
