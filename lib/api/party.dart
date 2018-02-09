cbe3280451f2e31e824e4ccc561936bcf2e48936
Chance Snow <git@chancesnow.me>
2018-02-09 02:34:59 -0800

    Add start party API

--- lib/api/party.dart
+++ lib/api/party.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:party/models/party.dart' as models;
 import 'package:party/api/base.dart';
@@ -11,6 +12,20 @@ class Party extends Endpoint {
   Future<models.Party> get() async {
     try {
       var json = await api.get(route('/party'));
+      return PartyMapper.fromJson(json);
+    } catch (_) {
+      return null;
+    }
+  }
+
+  Future<models.Party> start(String hostName, String playlistId) async {
+    try {
+      var bodyRaw = {
+        'host': hostName,
+        'playlist_id': playlistId
+      };
+      var body = JSON.encode({'data': bodyRaw});
+      var json = await api.post(route('/party/start'), body);
       return PartyMapper.fromJson(json);
     } catch (_) {
       return null;
